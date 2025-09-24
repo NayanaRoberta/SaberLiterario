084f7e72e8fb5fd35b1af86e601e800bdfe46c4f
Vitor Rafael <contatorafaelth@gmail.com>
2025-09-23 21:28:22 -0300

    removi arquivos desnecessários

--- app_autenticacao.py
+++
@@ -1,9 +0,0 @@
-
-# app_autenticacao.py
-
-def login(usuario, senha):
-    
-    if usuario == "usuario_valido@email.com" and senha == "senha_correta123":
-        return "sucesso"
-    else:
-        return "erro"
